2fa852f6712c68a652793c4e4f7a593ebd68c5ec
shepherdboy <1196220325@qq.com>
2022-05-23 03:44:18 +0800

    normal

--- app/src/main/java/com/shepherdboy/pdstreamline/activities/SettingActivity.java
+++ app/src/main/java/com/shepherdboy/pdstreamline/activities/SettingActivity.java
@@ -2,6 +2,8 @@ package com.shepherdboy.pdstreamline.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +12,24 @@ import com.shepherdboy.pdstreamline.R;
 
 public class SettingActivity extends AppCompatActivity {
 
+    private static Intent preIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        MyApplication.initActionBar(getSupportActionBar());
+        TextView textView = getSupportActionBar().getCustomView().findViewById(R.id.setting);
+
+        textView.setText("应用");
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingActivity.this.finish();
+            }
+        });
     }
 
     public static void actionStart() {
-
         MyApplication.getContext().startActivity(new Intent(MyApplication.getContext(), SettingActivity.class));
     }
 }
